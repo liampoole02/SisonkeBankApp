1fd333512c287e5c6b04ede14dea960ac15a027d
LIAM <liampoole02@gmail.com>
2020-09-02 20:50:30 +0300

    Logout on click done

--- app/src/main/java/com/example/liam/sisonkebankapp/MainPageActivity.java
+++ app/src/main/java/com/example/liam/sisonkebankapp/MainPageActivity.java
@@ -1,6 +1,8 @@
 package com.example.liam.sisonkebankapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +24,14 @@ public class MainPageActivity extends AppCompatActivity {
         btnviewbalance=findViewById(R.id.mainpageviewbalance);
         btntransfer=findViewById(R.id.mainpagetransfer);
         btnlogout=findViewById(R.id.mainpagelogout);
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainPageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
